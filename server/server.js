de48863144e7a6c5e6a78184d214c83e29136b87
hemlalit <hemlalitmali@015gmail.com>
2024-11-21 20:02:59 +0530

    deploying on vercel

--- server/server.js
+++ server/server.js
@@ -1,17 +1,10 @@
-require('dotenv').config();
-require('./config/passportConfig');
-
-const express = require('express');
 const { MongoClient, ServerApiVersion } = require("mongodb");
-// const mongoose = require('mongoose');
+const express = require('express');
 const passport = require('passport');
 const cors = require('cors');
 
-const corsConfig = {
-  origin: "*",
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-};
+require('dotenv').config();
+require('./config/passportConfig');
 
 const authRoutes = require('./routes/authRoutes');
 const profileRoutes = require('./routes/profileRoutes');
@@ -19,7 +12,13 @@ const expenseRoutes = require('./routes/expenseRoutes');
 
 const app = express();
 
-const uri = process.env.MONGO_URI || "mongodb+srv://onlyprogramming123:hemlalit15%40mongo@cluster0.fk6td3g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+};
+
+const uri = process.env.MONGO_URI || "your_default_uri_here";
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -29,30 +28,26 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
+async function connectMongoDB() {
   try {
-    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     console.log("You successfully connected to MongoDB!");
-
-    app.options("", cors(corsConfig));
-    app.use(cors(corsConfig));
-
-    // Middleware
-    app.use(express.json());
-    app.use(passport.initialize());
-
-    // Routes
-    app.use('/auth', authRoutes);
-    app.use('/profile', profileRoutes);
-    app.use('/expenses', expenseRoutes);
-
-    // Start the server after a successful MongoDB connection
-    app.listen(5000, () => console.log('Server started on port http://localhost:5000'));
-
   } catch (err) {
-    console.error('Error connecting to MongoDB:', err);
+    console.error("Error connecting to MongoDB:", err);
+    throw err; // Exit if MongoDB connection fails
   }
 }
-run().catch(console.dir);
+
+app.use(cors(corsConfig));
+app.use(express.json());
+app.use(passport.initialize());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/profile', profileRoutes);
+app.use('/api/expenses', expenseRoutes);
+
+connectMongoDB();
+
+// Export for Vercel
+module.exports = app;
