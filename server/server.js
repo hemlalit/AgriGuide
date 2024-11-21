eefa139b6248cd26332ef2f337511e1b52fbc19c
hemlalit <hemlalitmali@015gmail.com>
2024-11-21 18:01:29 +0530

    deploying on vercel

--- server/server.js
+++ server/server.js
@@ -2,15 +2,16 @@ require('dotenv').config();
 require('./config/passportConfig');
 
 const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport')
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// const mongoose = require('mongoose');
+const passport = require('passport');
 const cors = require('cors');
 
 const corsConfig = {
   origin: "*",
-  credential: "true",
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}
+};
 
 const authRoutes = require('./routes/authRoutes');
 const profileRoutes = require('./routes/profileRoutes');
@@ -18,24 +19,40 @@ const expenseRoutes = require('./routes/expenseRoutes');
 
 const app = express();
 
-app.options("", cors(corsConfig))
-app.use(cors(corsConfig))
+const uri = process.env.MONGO_URI || "mongodb+srv://onlyprogramming123:hemlalit15%40mongo@cluster0.fk6td3g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// Middleware
-app.use(express.json());
-app.use(passport.initialize());
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-// Routes
-app.use('/auth', authRoutes);
-app.use('/profile', profileRoutes);
-app.use('/expenses', expenseRoutes);
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    console.log("You successfully connected to MongoDB!");
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('MongoDB connected')
-    app.listen(5000, () => console.log('Server started on port http://localhost:5000')); // Start Server
-  })
-  .catch((err) => console.log(err));
+    app.options("", cors(corsConfig));
+    app.use(cors(corsConfig));
 
+    // Middleware
+    app.use(express.json());
+    app.use(passport.initialize());
 
+    // Routes
+    app.use('/auth', authRoutes);
+    app.use('/profile', profileRoutes);
+    app.use('/expenses', expenseRoutes);
+
+    // Start the server after a successful MongoDB connection
+    app.listen(5000, () => console.log('Server started on port http://localhost:5000'));
+
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+  }
+}
+run().catch(console.dir);
